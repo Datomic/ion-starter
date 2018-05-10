55d3e46f9d475f25143419cb0df6892e7b3edb4e
Stuart Halloway <stu@cognitect.com>
2018-05-10 08:19:41 -0400

    user.clj

---
+++ siderail/user.clj
@@ -0,0 +1,6 @@
+(require
+ '[clojure.data.json :as json]
+ '[clojure.pprint :as pp]
+ '[datomic.client.api :as d]
+ '[datomic.ion.starter :as starter]
+ '[datomic.ion.starter.examples.tutorial :as tutorial])
